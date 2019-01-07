a4e4c7804ce2479615e73b9191fe6acd44ad122d
zyc <billyzyc65535@gmail.com>
2019-01-07 13:28:53 +0000

    a button

--- pnchln.py
+++ pnchln.py
@@ -2,7 +2,8 @@
 
 from config import TELEGRAM_BOT_TOKEN as token
 from telegram.ext import Updater, InlineQueryHandler
-from telegram import InlineQueryResultArticle
+from telegram import InlineQueryResultArticle, InputTextMessageContent, InlineKeyboardButton, InlineKeyboardMarkup
+import logging
 
 def main():
     updater = Updater(token=token)
@@ -11,7 +12,27 @@ def main():
     updater.start_polling()
 
 def handler(bot, update):
-    pass
+    message = update.inline_query.query
+    button = InlineKeyboardButton(text = "theButton", url = "http://google.com")
+    retMsg = [InlineQueryResultArticle(
+        id=message.upper(),
+        title='haha',
+        reply_markup = InlineKeyboardMarkup([[button]]), 
+        input_message_content=InputTextMessageContent("ahah")
+    )]
+    bot.answer_inline_query(update.inline_query.id, retMsg)
+
+def removeAllHandlers(disp):
+    for group in disp.handlers.items:
+        for handler in group:
+            disp.remove_handler(handler)
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+updater = Updater(token=token)
+dispatcher = updater.dispatcher
+dispatcher.add_handler(InlineQueryHandler(handler))
 
 if __name__ == '__main__':
-    main()
+    pass
+#    main()
