a1c48f990da43d069f39e9486d5dfc0ad8516994
zyc <billyzyc65535@gmail.com>
2019-01-08 02:05:18 +0800

    roughly done, send to server

--- pnchln.py
+++ pnchln.py
@@ -1,37 +1,62 @@
 #!/usr/bin/env python3
 
 from config import TELEGRAM_BOT_TOKEN as token
-from telegram.ext import Updater, InlineQueryHandler
-from telegram import InlineQueryResultArticle, InputTextMessageContent, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, InlineQueryHandler, CallbackQueryHandler
+from telegram import InlineQueryResultArticle, InputTextMessageContent, \
+    InlineKeyboardButton, InlineKeyboardMarkup
+
 import logging
 
 def main():
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
     updater = Updater(token=token)
     dispatcher = updater.dispatcher
-    dispatcher.add_handler(InlineQueryHandler(handler))
+    dispatcher.add_handler(InlineQueryHandler(inlineHandler))
+    dispatcher.add_handler(CallbackQueryHandler(callbackHandler))
     updater.start_polling()
 
-def handler(bot, update):
-    message = update.inline_query.query
-    button = InlineKeyboardButton(text = "theButton", url = "http://google.com")
+#inl = 0
+def inlineHandler(bot, update):
+    #global inl
+    inl = update.inline_query
+    if not inl.query:
+        return
+    message = inl.query
+    button = InlineKeyboardButton(
+        text = "Reveal", callback_data = message + chr(0))
     retMsg = [InlineQueryResultArticle(
-        id=message.upper(),
-        title='haha',
-        reply_markup = InlineKeyboardMarkup([[button]]), 
-        input_message_content=InputTextMessageContent("ahah")
+        id = message.upper(),
+        title = "Hidden message",
+        reply_markup = InlineKeyboardMarkup([[button]]),
+        input_message_content = InputTextMessageContent("----------")
     )]
-    bot.answer_inline_query(update.inline_query.id, retMsg)
+    bot.answer_inline_query(inl.id, retMsg)
 
-def removeAllHandlers(disp):
-    for group in disp.handlers.items:
-        for handler in group:
-            disp.remove_handler(handler)
+#cb = 0
+def callbackHandler(bot, update):
+    #global cb
+    cb = update.callback_query
+    if not cb:
+        return
+    callback_data = cb.data
+    msg = callback_data[:-1]
+    count = ord(callback_data[-1])
+    if count == 0: # first time pressing
+        count = 1
+        buttonText = "Hide Again"
+        messageText = msg
+    elif count == 1:
+        count = 0
+        buttonText = "Reveal"
+        messageText = "----------"
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
-updater = Updater(token=token)
-dispatcher = updater.dispatcher
-dispatcher.add_handler(InlineQueryHandler(handler))
+    button = InlineKeyboardButton(
+        text = buttonText, callback_data = msg + chr(count))
+    update.callback_query.edit_message_text(
+        text = msgText,
+        reply_markup = InlineKeyboardMarkup([[button]])
+    )
 
 if __name__ == '__main__':
     pass
